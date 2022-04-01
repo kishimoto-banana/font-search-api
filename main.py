fdfc9d5985c0a179c8690818556c295e63239e13
masashi kishimoto <drehbleistift@gmail.com>
2022-04-01 20:31:19 +0900

    とりあえずのcors

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from lib2to3.pytree import Base
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
+app = FastAPI()
+
 
 class Image(BaseModel):
     name: str
     content: str
 
-app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/v1/fonts/")
 async def predict_fonts(image: Image):
     return image.dict()
-
