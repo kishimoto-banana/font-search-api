245077eb06eeeea537e1bfd010442798ef370b9b
masashi kishimoto <drehbleistift@gmail.com>
2022-05-20 15:48:33 +0900

    どうにかはしたい

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,7 +3,7 @@ import pickle
 NUM_TOP_K = 5
 PATCH_SIZE = 105
 PATCH_MARGIN = 3
-MODEL_PATH = "models/char_font365_vgg16_checkpoint.pt"
+MODEL_PATH = "models/char_font365_vgg16.pt"
 
 try:
     with open("models/meta.pkl", mode="rb") as f:
